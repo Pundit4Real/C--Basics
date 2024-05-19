936549973168268ddeb5fb45da9045ae279ee26c
Mohammed Ali <mohammedaalli088@gmail.com>
2024-05-19 08:50:31 +0000

    feat: Language Integrated Query done

---
+++ LINQ/Program.cs
@@ -0,0 +1,22 @@
+﻿// Specify the data source.
+int[] scores = [97, 92, 81, 60,100,85,94];
+
+
+// Define the query expression.
+IEnumerable<string> scoreQuery =
+    from score in scores
+    where score > 80
+    orderby score descending
+    select $"The score is {score}";
+
+Console.WriteLine(scoreQuery.Count());
+
+
+
+// Execute the query.
+foreach (var i in scoreQuery)
+{
+    Console.Write(i);
+}
+
+// Output: 97 92 81
